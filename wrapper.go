69152a7608104675c0b8123025911146ce8b315a
Manfred Touron <m@42.am>
2014-12-13 01:13:51 +0100

    wip

--- wrapper.go
+++ wrapper.go
@@ -1,22 +1,13 @@
 package main
 
 import (
-  "flag"
   "fmt"
   "os"
   "syscall"
 )
 
-func usage() {
-    fmt.Fprintf(os.Stderr, "usage: wrapper binary [args...]\n")
-    flag.PrintDefaults()
-    os.Exit(2)
-}
-
 func main() {
-    flag.Usage = usage
-    flag.Parse()
-    args := flag.Args()
+    args := os.Args
 
     if len(args) < 1 {
        fmt.Println("Input file is missing.");
@@ -24,7 +15,8 @@ func main() {
     }
 
     binary := "/usr/bin/qemu-arm-static"
-    args = append([]string{binary, "/bin/busybox", "sh", "-c"}, args...)
+    fmt.Println(args)
+    args = append([]string{binary, "/bin/busybox"}, args[2:]...)
     fmt.Println(args)
     env := os.Environ()
     execErr := syscall.Exec(binary, args, env)
