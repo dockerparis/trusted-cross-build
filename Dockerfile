4275d0a4f2ae9a1cafb542f5d24e367509065385
Manfred Touron <m@42.am>
2014-12-13 10:10:32 +0100

    wip

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
-FROM armbuild/busybox:latest
+FROM armbuild/ubuntu:latest
 
 ADD qemu-arm-static/qemu-arm-static /usr/bin/qemu-arm-static
 ADD wrapper/wrapper-i386 /bin/sh
 
-#RUN sh -c 'echo Hello World !'
-#RUN echo Hello World !
-#RUN date
-#RUN echo 42
+RUN sh -c 'echo Hello World !'
+RUN echo Hello World !
+RUN date
+RUN echo 42
+
+CMD bash
--- wrapper/wrapper.go
+++ wrapper/wrapper.go
@@ -6,18 +6,40 @@ import (
   "syscall"
 )
 
+type Utsname syscall.Utsname
+
+func uname() (*syscall.Utsname, error) {
+     uts := &syscall.Utsname{}
+
+     if err := syscall.Uname(uts); err != nil {
+     	return nil, err
+	}
+	return uts, nil
+}
+
 func main() {
     args := os.Args
 
     if len(args) < 1 {
-       fmt.Println("Input file is missing.");
+       fmt.Println("Input file is missing.")
        os.Exit(1);
     }
+
+    uts, _ := uname()
+    Machine := ""
+    for _, c := range uts.Machine { 
+        if c == 0 { 
+            break 
+        } 
+        Machine += string(byte(c)) 
+    } 
+    fmt.Println(Machine)
 
     binary := "/usr/bin/qemu-arm-static"
     fmt.Println(args)
     //args = append([]string{binary, "/bin/busybox"}, args[2:]...)
-    args = []string{binary, "/bin/busybox", "ls"}
+    args = append([]string{binary, "/bin/bash", "-c"}, args[2:]...)
+    //args = []string{binary, "/bin/busybox", "ls"}
     fmt.Println(args)
     env := os.Environ()
     execErr := syscall.Exec(binary, args, env)
